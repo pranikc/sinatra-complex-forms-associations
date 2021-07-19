fcfed451de6fa0cfcad24ece5c7b5352f598f8ee
Pranik Chainani <pranikc@gmail.com>
2021-07-19 01:49:14 -0400

    commit tree ab3dccf62ab048f02328434464136dc8f53ddb54

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,18 +2,24 @@ class OwnersController < ApplicationController
 
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
   end
 
   get '/owners/new' do 
+    @pets = Pet.all # need to iterate over for checkboxes
     erb :'/owners/new'
   end
 
   post '/owners' do 
-    
+    @owner = Owner.create(params[:owner])
+    if !params[:pet][:name].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
+    end
+    redirect :"/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
+    @pets = Pet.all
     @owner = Owner.find(params[:id])
     erb :'/owners/edit'
   end
@@ -24,6 +30,11 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do 
-   
+    @owner = Owner.find(params[:id])
+    @owner.update(params[:owner])
+    if !params[:pet][:name].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
+    end
+    redirect :"/owners/#{@owner.id}"
   end
 end
